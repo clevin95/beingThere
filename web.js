049f6341e4af41fca7a6b6b5224f1d3f013eff90
Carter Levin <carter.levin@flatironschool.com>
2014-07-14 08:20:43 -0400

    switched to http server

--- web.js
+++ web.js
@@ -1,3 +1,34 @@
+var http = require('http')
+var url = require('url')
+
+var server = http.createServer(function (request, response){
+	var requestType = request['method']
+	if (requestType == 'GET'){
+		console.log(request['method'])
+
+	}
+	else if (requestType == 'POST'){
+		request.on("data", function (chunk){
+			console.log(chunk.toString())
+		})
+	}
+	response.writeHead(200, {'Content-Type':'Json'})
+	var pathname = url.parse(request.url).pathname
+})
+
+
+var port = Number(process.env.PORT || 5000);
+server.listen(port, function() {
+	console.log("Listening on " + port);
+});
+
+
+
+
+
+
+
+/*
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
@@ -5,10 +36,11 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
+*/

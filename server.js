e7370050f6d9b698026aa349b46abccf5a98627b
Carter Levin <carter.levin@flatironschool.com>
2014-07-15 11:44:19 -0400

    temp commit

--- server.js
+++ server.js
@@ -5,31 +5,27 @@ var pg = require('pg');
 function start(route){
 	var server = http.createServer(function (request, response){
 		response.writeHead(200, {'Content-Type':'application/json'});
-        response.write('write to sight\n');
-		console.log("connected");
 		var method = request.method;
 		var	reqUrl = request.url;
 		var path = url.parse(reqUrl).pathname;
-		
+
+		var testArray = ['hello','this','is','an','array'];
+		var jsonArray = JSON.stringify(testArray);
+		response.write(jsonArray);
 		//route(path);
 		//response.write(request)
 		request.on("data", function(chunk){
-			console.log(chunk.toString());
-			response.write(chunk.toString())
-            
+			//console.log(chunk.toString());
+			//response.write(chunk.toString()) 
 		})
 		request.on("end", function(end){
-            response.write('write to sight');
 			response.end();
 		});
 	});
 	var port = Number(process.env.PORT || 5000);
 	server.listen(port, function() {
 		console.log("Listening on " + port);
-        
 	});
 }
 
 exports.start = start
-
-
